e1d45444f7bf5a09db36c158127d904723f781ef
Piyush Prateek <piyushprateek231996@gmail.com>
2023-10-14 01:55:36 +0530

    fixing bug

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,8 +6,8 @@ const app: Application = express()
 // Using Middlewares
 app.use(express.json({ limit: "50mb" }))
 app.use(cookieParser())
-// app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }))
 
 // Importing Routes
 import post from '../routes/postRoute'
