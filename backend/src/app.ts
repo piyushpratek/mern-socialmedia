9226eded4bf21e79ef8a433127ca8392b4151c72
Piyush Prateek <piyushprateek231996@gmail.com>
2023-10-13 23:49:58 +0530

    added command for render in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // Importing Routes
 import post from '../routes/postRoute'
 import user from '../routes/userRoute'
+import path from 'path'
 
 // const user = require("./routes/user");
 
@@ -27,6 +28,22 @@ app.use("/api/v1", user);
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 // });
+
+// Render Code
+if (process.env.USE_STATIC_BUILD === 'true') {
+    const reactBuildPath = path.join('./react-static')
+    const staticMiddleware = express.static(reactBuildPath)
+    app.use(staticMiddleware)
+    app.use('*', staticMiddleware)
+
+    const assetsPath = path.join('./react-static/assets')
+    app.use('/assets', express.static(assetsPath))
+} else {
+    // Redirect to /api/health
+    app.use('*', (req, res) => {
+        res.redirect('/api/health')
+    })
+}
 
 
 export default app
